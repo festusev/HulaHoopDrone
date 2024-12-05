df65cdd6ea34ace78127c50c3080a1ed9221979e
Evan Ellis <evanellis@wifi-10-41-140-116.wifi.berkeley.edu>
2024-12-04 16:45:49 -0800

    Adding lsq

--- fly_through_hoop.py
+++ fly_through_hoop.py
@@ -2,6 +2,8 @@ from djitellopy import Tello
 import cv2, math, time
 import rospy
 from std_msgs.msg import String
+from geometry_msgs.msg import TransformStamped
+import numpy as np
 
 tello = Tello()
 tello.connect()
@@ -14,28 +16,51 @@ tello.takeoff()
 hoop_cm = []
 drone_cm = []
 
+capturing = False
+
 def hoop_callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
+    if not capturing:
+        return
+
+    t = data.header.stamp
+    x = data.translation.x
+    y = data.translation.y
+    z = data.translation.z
+    hoop_cm.append([x, y, z, t])
+
+    if len(hoop_cm) > 5:
+        params = lsq(hoop_cm)
+        breakpoint()
+
+def lsq(cms):
+    cms = np.array(cms)
+    X = cms[:, :-1]
+    t = cms[:, -1]
+    Y = np.hstack([t**2 / 2, t, 1])
+
+    params = np.linalg.inv(X.T @ X) @ X.T @ Y
+    return params
+
 def hoop_listener():
-    rospy.init_node('hoop_listener', anonymous=True)
-    rospy.Subscriber("chatter", String, hoop_callback)
+    rospy.Subscriber("chatter", TransformStamped, hoop_callback)
     rospy.spin()
-
-    cm = [0, 0, 0]
-
-    hoop_cm.append(cm)
 
 def drone_callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
 def drone_listener():
-    rospy.init_node('drone_listener', anonymous=True)
-    rospy.Subscriber("chatter", String, drone_callback)
+    rospy.Subscriber("chatter", TransformStamped, drone_callback)
 
 if __name__ == '__main__':
+    rospy.init_node('flyer', anonymous=True)
     hoop_listener()
     drone_listener()
+
+    start = input("Type 'Y' to start").lower()
+    if start == "y" or start == "yes":
+        capturing = True
 
     tello.go_xyz_speed(200, 0, 0, 50)
     # tello.curve_xyz_speed(100, 100, 0, 200, 0, 0, 50)
